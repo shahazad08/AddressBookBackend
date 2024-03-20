4412eead57e71e7f4cb5692c3bfddeb6f9729346
unknown <shaikh.shahazad@bridgelabz.com>
2024-03-20 13:07:21 +0530

    added: New Exception and Refactor some previous

--- src/main/java/com/bridgelabz/addressbookapp/exceptions/ExceptionHandler.java
+++ src/main/java/com/bridgelabz/addressbookapp/exceptions/ExceptionHandler.java
@@ -1,39 +1,39 @@
-package com.bridgelabz.addressbookapp.exceptions;
-
-import com.bridgelabz.addressbookapp.Dto.ResponseDTO;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@ControllerAdvice
-public class ExceptionHandler {
-    private static final String message = "Exception While Processing REST Request";
-
-    // Exception is Handled for the Create Request
-    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception) {
-        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
-        List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage())
-                .collect(Collectors.toList());
-        ResponseDTO responseDTO = new ResponseDTO(message, errMesg);
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
-    }
-
-    // Exception is Handled for the Get Data By Id
-    @org.springframework.web.bind.annotation.ExceptionHandler(AddressBookException.class)
-    public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
-            AddressBookException exception) {
-        ResponseDTO responseDTO = new ResponseDTO("Exception while REST Request",
-                exception.getMessage());
-        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
-    }
-
-}
+//package com.bridgelabz.addressbookapp.exceptions;
+//
+//import com.bridgelabz.addressbookapp.Dto.ResponseDTO;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.FieldError;
+//import org.springframework.validation.ObjectError;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@ControllerAdvice
+//public class ExceptionHandler {
+//    private static final String message = "Exception While Processing REST Request";
+//
+//    // Exception is Handled for the Create Request
+//    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+//            MethodArgumentNotValidException exception) {
+//        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+//        List<String> errMesg = errorList.stream().map(objErr -> objErr.getDefaultMessage())
+//                .collect(Collectors.toList());
+//        ResponseDTO responseDTO = new ResponseDTO(message, errMesg);
+//        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+//    }
+//
+//    // Exception is Handled for the Get Data By Id
+//    @org.springframework.web.bind.annotation.ExceptionHandler(AddressBookException.class)
+//    public ResponseEntity<ResponseDTO> handleEmployeePayrollException(
+//            AddressBookException exception) {
+//        ResponseDTO responseDTO = new ResponseDTO("Exception while REST Request",
+//                exception.getMessage());
+//        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+//    }
+//
+//}
